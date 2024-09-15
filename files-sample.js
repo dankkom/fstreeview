5fcb375b65bcb45f9aa6140c6d3c73e934bf7811
Daniel Komesu <danielkomesu@gmail.com>
2024-09-15 11:35:18 -0300

    Add more items to files-sample.js

--- files-sample.js
+++ files-sample.js
@@ -1,10 +1,14 @@
 fileMetadata = [
-    {
-        "directory": "dir1/dir2",
-        "filename": "file1.txt"
-    },
-    {
-        "directory": "dir1/dir2/dir3",
-        "filename": "file2.txt"
-    }
+    { "directory": "dir1/dir2", "filename": "file1.txt" },
+    { "directory": "dir1/dir2/dir3", "filename": "file2.txt" },
+    { "directory": "dir1", "filename": "image.jpg" },
+    { "directory": ".", "filename": "index.html" },
+    { "directory": "dir4/subdir", "filename": "document.pdf" },
+    { "directory": "dir5", "filename": "script.js" },
+    { "directory": "downloads", "filename": "presentation.pptx" },
+    { "directory": "music", "filename": "song.mp3" },
+    { "directory": "videos", "filename": "movie.mp4" },
+    { "directory": "documents/reports", "filename": "report_2023.xlsx" },
+    { "directory": "projects/website", "filename": "style.css" },
+    { "directory": "archive", "filename": "backup.zip" }
 ]
